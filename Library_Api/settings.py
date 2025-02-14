16f3281c2b464fdd60447a7fab98df9d722e98eb
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-14 22:09:55 +0200

    feat: Added settings for drf-spectacular

--- Library_Api/settings.py
+++ Library_Api/settings.py
@@ -32,6 +32,10 @@ INSTALLED_APPS = [
     'Library_Api.library',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
