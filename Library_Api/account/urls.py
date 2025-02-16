8a16dbaed93aad5b6e586d1c1027e7affa4fdafa
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-16 23:32:23 +0200

    feat: Added logout view

--- Library_Api/account/urls.py
+++ Library_Api/account/urls.py
@@ -4,4 +4,5 @@ from Library_Api.account import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- Library_Api/settings.py
+++ Library_Api/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ] + PROJECT_APPS
 
 REST_FRAMEWORK = {
