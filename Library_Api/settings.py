a7635e41f325900fc0de0140c0030bcc47e344ce
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-16 22:04:43 +0200

    feat: Config JWT and Swagger

--- Library_Api/settings.py
+++ Library_Api/settings.py
@@ -35,11 +35,12 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Library API',
-    'DESCRIPTION': 'Books',
+    'DESCRIPTION': 'This application allows you to create and manage your Library APIs.',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
