cf83093deeef0f592836fda9abf6e78c32eafc1b
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-17 09:57:11 +0200

    feat: Added refresh token url

--- Library_Api/account/urls.py
+++ Library_Api/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from Library_Api.account import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
